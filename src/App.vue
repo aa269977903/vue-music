bb2c743ce44e8d2984acfbef2397e65f3f1d74ca
cest <786638185@qq.com>
2019-08-18 18:22:56 +0800

    '添加导航栏'

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <div id="app">
-    hello world
+    <m-header></m-header>
+    <tab></tab>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import MHeader from './components/m-header/m-header'
+import Tab from './components/tab/tab'
+
 export default {
-  name: 'App'
+  components: {
+    MHeader,
+    Tab
+  }
 }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  @import "~common/stylus/variable"
 </style>
